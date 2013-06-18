c5d373d612410f1b51914d013e62040039362be4
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-17 22:47:57 -0400

    first

---
+++ tinylisp.js
@@ -0,0 +1,89 @@
+;(function(exports) {
+  var Ctx = function() {
+    this.first = function(x) {
+      return x[0];
+    };
+
+    this.rest = function(x) {
+      return x.slice(1);
+    };
+
+    this.print = function() {
+      var argsArray = Array.prototype.slice.call(arguments);
+      console.log(argsArray);
+      return argsArray;
+    };
+  };
+
+  var interpret = function(input, ctx) {
+    if (ctx === undefined) {
+      return interpret(input, new Ctx());
+    } else if (input.value !== undefined) {
+      return input.value;
+    } else {
+      if (input[0].type === "identifier") {
+        return ctx[input[0].value].apply(this, interpret(input.slice(1)));
+      } else {
+        return input.slice(0).map(function(x) {
+          return interpret(x);
+        });
+      }
+    }
+  };
+
+  var atDepth = function(output, depth) {
+    if (depth === 0) {
+      return output;
+    } else {
+      return atDepth(output[output.length - 1], depth - 1);
+    }
+  };
+
+  var type = function(input) {
+    if (!isNaN(parseFloat(input))) {
+      return { type:'number', value: parseFloat(input) };
+    } else if (input[0] === '"' && input.slice(-1) === '"') {
+      return { type:'string', value: input.slice(1, -1) };
+    } else {
+      return { type:'identifier', value: input };
+    }
+  };
+
+  var parenthesize = function(input) {
+    var output = [];
+    var depth = 0;
+    while (input.length > 0) {
+      var token = input.shift();
+      if (token === "(") {
+        atDepth(output, depth++).push([]);
+      } else if (token === ")") {
+        depth--;
+      } else {
+        atDepth(output, depth).push(type(token));
+      }
+    }
+
+    return output.pop();
+  };
+
+  var tokenize = function(input) {
+    return input.split('"').map(function(x, i) {
+                   return i % 2 === 0 ? x : x.replace(/ /g, "!spaceholder!");
+                 }).join('"')
+                .replace(/\(/g, ' ( ')
+                .replace(/\)/g, ' ) ')
+                .trim()
+                .split(/ +/)
+                .map(function(x) {
+                  return x.replace(/!spaceholder!/, " ");
+                });
+  };
+
+  var parse = function(input) {
+    return parenthesize(tokenize(input));
+  };
+
+  exports.tinyLisp = {};
+  exports.tinyLisp.parse = parse;
+  exports.tinyLisp.interpret = interpret;
+})(typeof exports === 'undefined' ? this : exports);
---
+++ tinylisp.spec.js
@@ -0,0 +1,102 @@
+var t = require('./tinylisp').tinyLisp;
+
+var is = function(input, type) {
+  return Object.prototype.toString.call(input) === '[object ' + type + ']';
+};
+
+// takes an AST and replaces type annotated nodes with raw values
+var unannotate = function(input) {
+  if (is(input, 'Array')) {
+    if (input[0] === undefined) {
+      return [];
+    } else if (is(input[0], 'Array')) {
+      return [unannotate(input[0])].concat(unannotate(input.slice(1)));
+    } else {
+      return unannotate(input[0]).concat(unannotate(input.slice(1)));
+    }
+  } else {
+    return [input.value];
+  }
+};
+
+describe('tinyLisp', function() {
+  describe('parse', function() {
+    it('should lex an atom in a list', function() {
+      expect(unannotate(t.parse("()"))).toEqual([]);
+    });
+
+    it('should lex multi atom list', function() {
+      expect(unannotate(t.parse("(hi you)"))).toEqual(["hi", "you"]);
+    });
+
+    it('should lex list containing list', function() {
+      expect(unannotate(t.parse("((x))"))).toEqual([["x"]]);
+    });
+
+    it('should lex list containing list', function() {
+      expect(unannotate(t.parse("(x (x))"))).toEqual(["x", ["x"]]);
+    });
+
+    it('should lex list containing list', function() {
+      expect(unannotate(t.parse("(x y)"))).toEqual(["x", "y"]);
+    });
+
+    it('should lex list containing list', function() {
+      expect(unannotate(t.parse("(x (y) z)"))).toEqual(["x", ["y"], "z"]);
+    });
+
+    it('should lex list containing list', function() {
+      expect(unannotate(t.parse("(x (y) (a b c))"))).toEqual(["x", ["y"], ["a", "b", "c"]]);
+    });
+
+    describe('atoms', function() {
+      it('should parse out numbers', function() {
+        expect(unannotate(t.parse("(1 (a 2))"))).toEqual([1, ["a", 2]]);
+      });
+    });
+  });
+
+  describe('interpret', function() {
+    describe('lists', function() {
+      it('should return list of strings', function() {
+        expect(t.interpret(t.parse('("hi" "mary" "rose")'))).toEqual(['hi', "mary", "rose"]);
+      });
+
+      it('should return list of numbers', function() {
+        expect(t.interpret(t.parse('(1 2 3)'))).toEqual([1, 2, 3]);
+      });
+
+      it('should return list of numbers in strings as strings', function() {
+        expect(t.interpret(t.parse('("1" "2" "3")'))).toEqual(["1", "2", "3"]);
+      });
+    });
+
+    describe('atoms', function() {
+      it('should return string atom', function() {
+        expect(t.interpret(t.parse('"a"'))).toEqual("a");
+      });
+
+      it('should return string with space atom', function() {
+        expect(t.interpret(t.parse('"a b"'))).toEqual("a b");
+      });
+
+      it('should return number atom', function() {
+        expect(t.interpret(t.parse('123'))).toEqual(123);
+      });
+    });
+
+    describe('invocation', function() {
+      it('should run print on an int', function() {
+        expect(t.interpret(t.parse("(print 1)"))).toEqual([1]);
+      });
+
+      it('should return first element of list', function() {
+        expect(t.interpret(t.parse("(first (1 2 3))"))).toEqual(1);
+      });
+
+      it('should return rest of list', function() {
+        expect(t.interpret(t.parse("(rest (1 2 3))"))).toEqual([2, 3]);
+      });
+    });
+  });
+});
