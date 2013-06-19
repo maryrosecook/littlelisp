2a43324128934de03c5e6426f0917602a1336ad8
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-19 10:20:52 -0400

    Add tiny repl.

---
+++ repl.js
@@ -0,0 +1,15 @@
+var repl = require("repl");
+var tinyLisp = require("./tinyLisp").tinyLisp;
+
+repl.start({
+  prompt: "> ",
+  eval: function(cmd, context, filename, callback) {
+    if (cmd !== "(\n)") {
+      cmd = cmd.slice(1, -2); // rm parens and newline added by repl
+      var ret = tinyLisp.interpret(tinyLisp.parse(cmd));
+      callback(null, ret);
+    } else {
+      callback(null);
+    }
+  }
+});
