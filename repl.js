eedd2a18bf645b6d5f57a76636ffd095ef09d71f
Mary Rose Cook <mary@maryrosecook.com>
2018-01-09 13:24:44 +0000

    Fix REPL so it no longer removes read chars from user input
    * Node's REPL no longer sends the prompt and newline.
    * Thanks, Chris!

--- repl.js
+++ repl.js
@@ -4,12 +4,7 @@ var littleLisp = require("./littlelisp").littleLisp;
 repl.start({
   prompt: "> ",
   eval: function(cmd, context, filename, callback) {
-    if (cmd !== "(\n)") {
-      cmd = cmd.slice(1, -2); // rm parens and newline added by repl
-      var ret = littleLisp.interpret(littleLisp.parse(cmd));
-      callback(null, ret);
-    } else {
-      callback(null);
-    }
+    var ret = littleLisp.interpret(littleLisp.parse(cmd));
+    callback(null, ret);
   }
 });
